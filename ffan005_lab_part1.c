d2b06ef381c11dbdef8f59ea15fd53e6c3cddc10
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-31 17:51:07 -0700

    Update ffan005_lab_part1.c

--- ffan005_lab_part1.c
+++ ffan005_lab_part1.c
@@ -12,7 +12,9 @@
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
-enum States {Start, Init, Cnote, Dnote, Enote} state;
+
+enum States {Start, Init, PlayC, PlayD, PlayE} state;
+
 void set_PWM(double frequency) {
   static double current_frequency;
   if (frequency != current_frequency) {
@@ -40,64 +42,70 @@ void PWM_off() {
 void Tick() {
         switch(state) {
                 case Start:     
-			state = Init; break;
+		state = Init; 
+		break;
 
                 case Init:      
-			if ((~PINA & 0x07) == 0x01) {
-			state = PlayC;
-			}else if ((~PINA & 0x07) == 0x02) {
-			state = PlayD;
-			}else if ((~PINA & 0x07) == 0x04) {
-			state = PlayE;
-			} else {
-			state = Init;
-			}
-                        break;
+		if ((~PINA & 0x07) == 0x01) {
+		state = PlayC;
+		}else if ((~PINA & 0x07) == 0x02) {
+		state = PlayD;
+		}else if ((~PINA & 0x07) == 0x04) {
+		state = PlayE;
+		} else {
+		state = Init;
+		}
+                break;
 
                 case PlayC:     
-			if ((~PINA & 0x07) == 0x01) {
-			state = PlayC;
-			}else {
-			state = Init;
-			}
-                        break;
+		if ((~PINA & 0x07) == 0x01) {
+		state = PlayC;
+		}else {
+		state = Init;
+		}
+                break;
 
                 case PlayD:     
-			if ((~PINA & 0x07) == 0x02) {
-			state = PlayD;
-			}else {
-			state = Init;
-			}
-                        break;
+		if ((~PINA & 0x07) == 0x02) {
+		state = PlayD;
+		}else {
+		state = Init;
+		}
+                break;
 
                 case PlayE:     
-			if ((~PINA & 0x07) == 0x04) {
-			state = PlayE;
-			}else {
-			state = Init;
-			}
-                        break;
-
+		if ((~PINA & 0x07) == 0x04) {
+		state = PlayE;
+		}else {
+		state = Init;
+		}
+                break;
+			
                 default:        
-			state = Start; 
-			break;
+		state = Start; 
+		break;
         }
 
         switch (state) {
                 case Start:     
-			break;
+		break;
+			
                 case Init:      
-			set_PWM(0); 
-			break;
+		set_PWM(0); 
+		break;
+			
                 case PlayC:     
-			set_PWM(261.63); 
-			break;
+		set_PWM(261.63); 
+		break;
+			
                 case PlayD:     
-			set_PWM(293.66); 
-			break;
+		set_PWM(293.66); 
+		break;
+			
                 case PlayE:     
-			set_PWM(329.63); 
-			break;
+		set_PWM(329.63); 
+		break;
+			
                 default:        
 			break;
         }
@@ -106,7 +114,9 @@ void Tick() {
 int main(void) {
     DDRA = 0x00; PORTA = 0xFF;
     DDRB = 0xFF; PORTB = 0x00;
+	
     PWM_on();
+	
     while (1) {
         Tick();
     }
