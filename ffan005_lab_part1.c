cb1ee3326467ef7cc0dfe05fb8afb501482fd4f7
FengchunFan <77016886+FengchunFan@users.noreply.github.com>
2021-10-31 17:46:33 -0700

    Update ffan005_lab_part1.c

--- ffan005_lab_part1.c
+++ ffan005_lab_part1.c
@@ -8,6 +8,35 @@
  *      code, is my own original work.
  *      Demo:
  */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+enum States {Start, Init, Cnote, Dnote, Enote} state;
+void set_PWM(double frequency) {
+  static double current_frequency;
+  if (frequency != current_frequency) {
+    if (!frequency) { TCCR3B &= 0x08; }
+    else { TCCR3B |= 0x03; }
+    if (frequency < 0.954) { OCR3A = 0xFFFF; }
+    else if (frequency > 31250) { OCR3A = 0x0000; }
+    else { OCR3A = (short) (8000000 / (128 * frequency)) - 1; }
+    TCNT3 = 0;
+    current_frequency = frequency;
+  }
+}
+
+void PWM_on() {
+  TCCR3A = (1 << COM3A0);
+  TCCR3B = (1 << WGM32) | (1 << CS31) | (1 << CS30);
+  set_PWM(0);
+}
+
+void PWM_off() {
+        TCCR3A = 0x00;
+        TCCR3B = 0x00;
+}
+
 void Tick() {
         switch(state) {
                 case Start:     
@@ -25,28 +54,33 @@ void Tick() {
 			}
                         break;
 
-                case PlayC:     if ((~PINA & 0x07) == 0x01) {
-				state = PlayC;
-				}else {
-				state = Init;
-				}
-                                break;
+                case PlayC:     
+			if ((~PINA & 0x07) == 0x01) {
+			state = PlayC;
+			}else {
+			state = Init;
+			}
+                        break;
 
-                case PlayD:     if ((~PINA & 0x07) == 0x02) {
-				state = PlayD;
-				}else {
-				state = Init;
-				}
-                                break;
+                case PlayD:     
+			if ((~PINA & 0x07) == 0x02) {
+			state = PlayD;
+			}else {
+			state = Init;
+			}
+                        break;
 
-                case PlayE:     if ((~PINA & 0x07) == 0x04) {
-				state = PlayE;
-				}else {
-				state = Init;
-				}
-                                break;
+                case PlayE:     
+			if ((~PINA & 0x07) == 0x04) {
+			state = PlayE;
+			}else {
+			state = Init;
+			}
+                        break;
 
-                default:        state = Start; break;
+                default:        
+			state = Start; 
+			break;
         }
 
         switch (state) {
